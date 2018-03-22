22e93a2701758c37bfd1154a3cad079fb848f1bd
Abhiram T <abhiram.att@gmail.com>
2018-03-22 17:43:36 +0530

    added payment

--- Assignment 22_3_18/src/MainMaster/Bank.java
+++ Assignment 22_3_18/src/MainMaster/Bank.java
@@ -1,7 +1,16 @@
 package MainMaster;
+import P1.*;
+import P2.*;
+import P3.*;
+import P4.*;
+import P5.*;
+import java.util.*;
 
-public class Bank {
-
+public class Bank extends Exception {
+	public Bank() {}
+	public Bank(String msg) {
+		super(msg);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- Assignment 22_3_18/src/P5/Payment.java
+++ Assignment 22_3_18/src/P5/Payment.java
@@ -1,5 +1,36 @@
 package P5;
 
-public class Payment {
+import P2.Balance;
+
+@SuppressWarnings("serial")
+public class Payment extends Exception {
+	public Payment() {}
+	public Payment(String msg) {
+		super(msg);
+	}
+	public Balance x;
+	public Payment(Balance x) {
+		this.x=x;
+	}
+	public void Pay(float amt) throws Payment {
+		try {
+			if(amt>x.bal) {
+				throw new Payment("INSUFFICIENT BALANCE");
+			}
+			else {
+				System.out .println("Amount Deposite="+amt);
+				x.bal=x.bal+amt;
+				x.display();
+				System.out .println("Transection Completed");
+			}
+		}
+		catch(Payment WD) {
+			throw WD;//Rethrow
+		}
+    }
 
 }
+
+
+
+
